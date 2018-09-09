908827519112d64e28626722721cbec4cb20d6ee
Johnathan Huggett <johnathanhuggett@Johnathans-MBP.local>
2018-09-09 18:59:31 -0400

    updated all classNames to BEM/ updated logo

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,26 +4,26 @@ import SvgButton from '../components/shared/SvgButton';
 import './index.css';
 
 export default ({ children }) => (
-  <div className="Background-Wrapper">
-    <header className="Social-Wrapper">
+  <div className="Background_Wrapper">
+    <header className="Social_Wrapper">
       {/* <SvgButton type="Email" to="https://www.google.com/" /> */}
       <SvgButton
-        className="Social-Button"
+        className="Social_Button"
         type="FaceBook"
         to="https://www.facebook.com/pg/stagstudiodesign/"
       />
       <SvgButton
-        className="Social-Button"
+        className="Social_Button"
         type="Instagram"
         to="https://www.instagram.com/jasontaylorinteriors/"
       />
       <SvgButton
-        className="Social-Button"
+        className="Social_Button"
         type="Twitter"
         to="https://twitter.com/search?f=tweets&q=DesignsStag"
       />
       <SvgButton
-        className="Social-Button"
+        className="Social_Button"
         type="Pinterest"
         to="https://www.pinterest.com/stagstudiodesigns/"
       />
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,24 @@ import Carousel from '../components/Carousel';
 import SvgButton from '../components/shared/SvgButton';
 import NavLink from '../components/shared/NavLink';
 import project_img from '../nav_carousel/projects.jpg';
-import blog_img from '../nav_carousel/living_room.jpg';
-import contact_img from '../nav_carousel/bedroom.jpg';
-// import about_img from '../nav_carousel/about.jpg';
+import blog_img from '../nav_carousel/fan.jpg';
+// import about_img from '../nav_carousel/deer.jpg';
+// import contact_img from '../nav_carousel/bedroom.jpg';
 
 import './index.css';
 
 export default () => (
   <div className="Container">
-    <div className="Nav-Container">
+    <div className="Nav_Container">
       <div className="Header">
         <span>JASON</span>
         <SvgButton className="Logo" type="Logo" to="/" />
         <span>TAYLOR</span>
       </div>
       <p>classic design, rooted in tradition, wistfully adorned.</p>
-      <div className="Nav-Menu">
-        <div className="Menu-Header">Navigation</div>
-        <ul className="Menu-Link-Wrapper">
+      <div className="Nav_Menu">
+        <div className="Nav_Menu_Header">Navigation</div>
+        <ul className="Nav_Menu_Link_Wrapper">
           <li>
             <NavLink className="NavLink" type="Projects" to="/projects" />
           </li>
@@ -37,8 +37,8 @@ export default () => (
         </ul>
       </div>
     </div>
-    <div className="Carousel-Container">
-      <Carousel images={[project_img, blog_img, contact_img]} />
+    <div className="Carousel_Container">
+      <Carousel images={[project_img, blog_img]} />
     </div>
   </div>
 );

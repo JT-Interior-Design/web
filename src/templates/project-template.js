12f023c22629ba0599d1ad8c4ed42bc55bfd442e
Ashwin <aswinjeganath@gmail.com>
2018-11-04 05:00:32 +0530

    Add brick layout

--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -1,43 +1,62 @@
 import React from 'react';
+import g from 'glamorous';
+import Bricks from 'bricks.js';
 
 import SubNav from '../components/navigation/subNav';
 
 import './project.css';
 
-export default ({ data }) => {
-  const {
-    cosmicjsProjects: {
-      title,
-      content,
-      metadata: {
-        images,
-        hero: { imgix_url },
+export default class Project extends React.Component {
+  componentDidMount() {
+    console.log(this.imageGrid);
+    this.instance = Bricks({
+      container: '.Project__brick',
+      sizes: [
+        { columns: 1, gutter: 3 },
+        { mq: '900px', columns: 2, gutter: 3 },
+        { mq: '1200px', columns: 3, gutter: 3 },
+      ],
+      packed: 'data-packed',
+    });
+    this.instance.resize(true).pack();
+  }
+
+  render() {
+    const { data } = this.props;
+
+    const {
+      cosmicjsProjects: {
+        title,
+        content,
+        metadata: {
+          images,
+          hero: { imgix_url },
+        },
       },
-    },
-  } = data;
+    } = data;
 
-  return (
-    <div className="Project__container">
-      <SubNav navLinks={['Home', 'Projects', 'Contact', 'About', 'News']} />
-      <h1>{title}</h1>
-      <img className="Project__Jumbo" src={imgix_url} alt={title} />
-      <div dangerouslySetInnerHTML={{ __html: content }} />
-      <div className="Project__Img__grid">
-        {images.map(({ image }, i) => (
-          <div style={{ width: '100%' }}>
-            <img
-              className="Project__Img"
-              key={i}
-              src={image.imgix_url}
-              alt={title}
-            />
-          </div>
-        ))}
+    return (
+      <div className="Project__container">
+        <SubNav navLinks={['Home', 'Projects', 'Contact', 'About', 'News']} />
+        <h1>{title}</h1>
+        <img className="Project__Jumbo" src={imgix_url} alt={title} />
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+        <div ref={node => (this.imageGrid = node)} className="Project__brick">
+          {images.map(({ image }, i) => (
+            <g.Div width={320}>
+              <img
+                className="Project__Img"
+                key={i}
+                src={image.imgix_url}
+                alt={title}
+              />
+            </g.Div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  }
+}
 export const pageQuery = graphql`
   query POST($id: String!) {
     cosmicjsProjects(id: { eq: $id }) {

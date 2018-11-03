490ae2c1897af1a4b3eec92a0438403c3d440283
Ashwin <aswinjeganath@gmail.com>
2018-11-04 05:04:01 +0530

    Add page transition to project-template

--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import g from 'glamorous';
 import Bricks from 'bricks.js';
+import PageTransition from 'gatsby-plugin-page-transitions';
 
 import SubNav from '../components/navigation/subNav';
 
@@ -36,24 +37,26 @@ export default class Project extends React.Component {
     } = data;
 
     return (
-      <div className="Project__container">
-        <SubNav navLinks={['Home', 'Projects', 'Contact', 'About', 'News']} />
-        <h1>{title}</h1>
-        <img className="Project__Jumbo" src={imgix_url} alt={title} />
-        <div dangerouslySetInnerHTML={{ __html: content }} />
-        <div ref={node => (this.imageGrid = node)} className="Project__brick">
-          {images.map(({ image }, i) => (
-            <g.Div width={320}>
-              <img
-                className="Project__Img"
-                key={i}
-                src={image.imgix_url}
-                alt={title}
-              />
-            </g.Div>
-          ))}
+      <PageTransition transitionTime={750}>
+        <div className="Project__container">
+          <SubNav navLinks={['Home', 'Projects', 'Contact', 'About', 'News']} />
+          <h1>{title}</h1>
+          <img className="Project__Jumbo" src={imgix_url} alt={title} />
+          <div dangerouslySetInnerHTML={{ __html: content }} />
+          <div ref={node => (this.imageGrid = node)} className="Project__brick">
+            {images.map(({ image }, i) => (
+              <g.Div width={320}>
+                <img
+                  className="Project__Img"
+                  key={i}
+                  src={image.imgix_url}
+                  alt={title}
+                />
+              </g.Div>
+            ))}
+          </div>
         </div>
-      </div>
+      </PageTransition>
     );
   }
 }

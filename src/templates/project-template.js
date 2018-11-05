f5f811d41ebe100d9b64001d94efbce411f6ae81
Ashwin <aswinjeganath@gmail.com>
2018-11-05 15:51:26 +0530

    Change brick layout implementation using CSS grids

--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import g from 'glamorous';
-import Bricks from 'bricks.js';
 import PageTransition from 'gatsby-plugin-page-transitions';
 
 import SubNav from '../components/navigation/subNav';
@@ -8,23 +6,41 @@ import SubNav from '../components/navigation/subNav';
 import './project.css';
 
 export default class Project extends React.Component {
+  setLandscape = () => {
+    if (this.imageGrid) {
+      const children = Array.from(this.imageGrid.children);
+
+      for (let div of children) {
+        const image = div.querySelector('img');
+        const landscape = image.naturalHeight < image.naturalWidth;
+        if (landscape) div.classList.add('landscape');
+      }
+    }
+  };
+
   componentDidMount() {
-    console.log(this.imageGrid);
-    this.instance = Bricks({
-      container: '.Project__brick',
-      sizes: [
-        { columns: 1, gutter: 3 },
-        { mq: '900px', columns: 2, gutter: 3 },
-        { mq: '1200px', columns: 3, gutter: 3 },
-      ],
-      packed: 'data-packed',
-    });
-    this.instance.resize(true).pack();
+    this.setLandscape();
+    // this.instance = Bricks({
+    //   container: '.Project__brick',
+    //   sizes: [
+    //     { columns: 1, gutter: 3 },
+    //     { mq: '900px', columns: 2, gutter: 3 },
+    //     { mq: '1200px', columns: 3, gutter: 3 },
+    //   ],
+    //   packed: 'data-packed',
+    // });
+    // this.instance.resize(true).pack();
   }
 
   componentDidUpdate() {
-    this.instance && this.instance.resize(true).pack();
+    // this.instance && this.instance.resize(true).pack();
   }
+
+  handleImageLoad = i => {
+    if (i === this.props.data.cosmicjsProjects.metadata.images.length - 1) {
+      this.setLandscape();
+    }
+  };
 
   render() {
     const { data } = this.props;
@@ -49,14 +65,15 @@ export default class Project extends React.Component {
           <div dangerouslySetInnerHTML={{ __html: content }} />
           <div ref={node => (this.imageGrid = node)} className="Project__brick">
             {images.map(({ image }, i) => (
-              <g.Div width={320}>
+              <div>
                 <img
                   className="Project__Img"
                   key={i}
                   src={image.imgix_url}
                   alt={title}
+                  onLoad={() => this.handleImageLoad(i)}
                 />
-              </g.Div>
+              </div>
             ))}
           </div>
         </div>

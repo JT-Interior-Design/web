8fb424057c9696be55fc19e3f3fdcc5c70bdbe35
Ashwin <aswinjeganath@gmail.com>
2018-11-10 23:02:25 +0530

    Refactor home page and loading for mobile

--- src/components/navigation/JTHeader.js
+++ src/components/navigation/JTHeader.js
@@ -4,14 +4,7 @@ import g from 'glamorous';
 export default ({ className }) => (
   <div>
     <div className={`${className}`}>
-      <g.H1
-        fontFamily={'"PT Serif", serif'}
-        marginTop={0}
-        fontSize={43}
-        marginBottom={6}
-      >
-        JASON TAYLOR
-      </g.H1>
+      <h1>JASON TAYLOR</h1>
     </div>
     <p>INTERIORS</p>
   </div>

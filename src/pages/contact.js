67e74aa3cc5343fc708f2d7f4d20b45698b981d4
johnathanhuggett <johnathan.huggett@amount.com>
2019-06-23 15:11:21 -0400

    form check

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,10 +6,16 @@ import ContactSidebar from '../components/contact/ContactSidebar';
 import ContactMain from '../components/contact/ContactMain';
 import SubNav from '../components/navigation/subNav';
 
+import '../components/contact/contact.css';
+
 export default class extends React.PureComponent {
-  state = { selected: 0 };
+  state = { selected: 0, openModal: false };
 
   changeSelected = value => this.setState({ selected: value });
+
+  displayModal = () => {
+    this.setState({ openModal: true });
+  };
 
   render() {
     return (
@@ -25,6 +31,46 @@ export default class extends React.PureComponent {
         }}
         transitionTime={750}
       >
+        {this.state.openModal ? (
+          <div className="contact-modal">
+            <div className="contact-modal__box">
+              <p className="contact-modal__text">
+                Thanks for reaching out we will get back to you soon!
+              </p>
+              <g.Button
+                type="submit"
+                border="none"
+                border="1px solid #9eca50"
+                color="white"
+                backgroundColor="#9eca50"
+                padding="10px 30px"
+                textTransform="uppercase"
+                borderRadius={4}
+                fontSize={16}
+                letterSpacing={3}
+                fontFamily="PT Serif"
+                cursor="pointer"
+                onClick={() =>
+                  this.setState({ openModal: !this.state.openModal })
+                }
+                css={{
+                  '&:hover': {
+                    backgroundColor: '#fff',
+                    color: '#9eca50',
+                  },
+                  '&:focus': {
+                    outline: 'none',
+                  },
+                  '&:active': {
+                    outline: 'none',
+                  },
+                }}
+              >
+                Return
+              </g.Button>
+            </div>
+          </div>
+        ) : null}
         <g.Div
           backgroundColor="rgba(255, 255, 255, 0.8)"
           flex={1}
@@ -53,7 +99,10 @@ export default class extends React.PureComponent {
               selected={this.state.selected}
               changeSelected={this.changeSelected}
             />
-            <ContactMain selected={this.state.selected} />
+            <ContactMain
+              selected={this.state.selected}
+              displayModal={this.displayModal}
+            />
           </g.Div>
         </g.Div>
       </PageTransition>
